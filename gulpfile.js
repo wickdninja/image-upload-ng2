a4bac95fa173f6bdf68f8334c8b97b0bbe5f1e66
Nate <nross63@gmail.com>
2016-04-14 02:49:03 -0400

    image capture basic app working

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var spawn = require('child_process').spawn;
+var electron = require('electron-prebuilt');
+var gulp = require('gulp');
+var jetpack = require('fs-jetpack');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var os = require('os');
+var release_windows = require('./buil.windows');
+var zip = require('gulp-vinyl-zip');
+var electron = require('gulp-atom-electron');
+var projectDir = jetpack;
+var srcDir = projectDir.cwd('./app');
+var destDir = projectDir.cwd('./build');
+var rimraf = require('gulp-rimraf');
+
+
+
+gulp.task('build-osx', ['build'], function () {
+    return gulp.src('build/**')
+        .pipe(electron({
+            version: '0.36.7',
+            platform: 'darwin',
+            darwinIcon: './resources/darwin/allied.icns',
+        }))
+        .pipe(zip.dest('IPay_Test_OSX.zip'));
+});
+
+gulp.task('build-linux', ['build'], function () {
+    return gulp.src('build/**')
+        .pipe(electron({ name: 'IPay Test UI', version: '0.36.7', platform: 'linux' }))
+        .pipe(zip.dest('IPay_Test_NIX.zip'));
+});
+
+gulp.task('build-win32', ['build'], function () {
+    return gulp.src('build/**')
+        .pipe(electron({
+            name: 'IPay Test UI',
+            version: '0.36.7',
+            platform: 'win32',
+            winIcon: './resources/windows/allied.ico',
+            companyName: 'Allied Payment',
+        }))
+        .pipe(zip.dest('IPay_Test_WIN.zip'));
+});
+
+
+gulp.task('release', ['build-win32','build-osx','build-linux']);
